1cb3eeb819483596c801d9f6357479cf41e7b7d8
Jai <jai.chhatre@gmail.com>
2020-07-12 11:08:05 -0700

    Updates

---
+++ precache-manifest.110784a245f764731ff6425daea4fbc2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c15a7e4d9530bddd572c45b2453c95fa",
+    "url": "/BasicCalculatorReact/index.html"
+  },
+  {
+    "revision": "ac989ac05447648706e1",
+    "url": "/BasicCalculatorReact/static/css/main.9a10dae0.chunk.css"
+  },
+  {
+    "revision": "7d9a2529d99160b9d1d8",
+    "url": "/BasicCalculatorReact/static/js/2.737dc4ad.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/BasicCalculatorReact/static/js/2.737dc4ad.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ac989ac05447648706e1",
+    "url": "/BasicCalculatorReact/static/js/main.acb6baab.chunk.js"
+  },
+  {
+    "revision": "b313574d4c5d37fb0402",
+    "url": "/BasicCalculatorReact/static/js/runtime-main.3f51335b.js"
+  }
+]);
